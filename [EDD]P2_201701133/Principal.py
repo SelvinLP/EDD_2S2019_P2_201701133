ab6c85050ff51553bda5bc777a21bbf3b90f443b
SelvinLP <36603066+SelvinLP@users.noreply.github.com>
2019-10-05 22:14:30 -0600

    Creacion del Proyecto

---
+++ [EDD]P2_201701133/Principal.py
@@ -0,0 +1,59 @@
+#Librerias
+#para instalar curses python -m pip install windows-curses
+import csv
+import curses
+from curses import KEY_RIGHT, KEY_LEFT, KEY_UP, KEY_DOWN
+import subprocess
+import os
+
+
+
+#Metodos de Curses
+stdscr = curses.initscr()
+TamañoTablero_y=20
+TamañoTablero_x=50
+window = curses.newwin(TamañoTablero_y,TamañoTablero_x,0,0)
+window.keypad(True)
+curses.noecho()
+curses.curs_set(0)
+window.nodelay(True)
+
+#variables del menu
+opcion =0
+
+def Espera_Salir(Vent):
+    key = Vent.getch()
+    while key!=8:#posicion para retroceder DELETE
+        key = Vent.getch()
+
+def Pintado_Menu(Vent):
+    Pintado_Titulo(Vent,' MENU PRINCIPAL ')
+    Vent.addstr(7,21, '1. INSERT BLOCK  ')#49
+    Vent.addstr(8,21, '2. SELECT BLOCK')#50
+    Vent.addstr(9,21, '3. REPORTS')#51
+    Vent.addstr(10,21, '4. EXIT')#52
+
+    Vent.timeout(-1)
+
+def Pintado_Titulo(Vent,cadena):
+    Vent.clear()
+    Vent.border(0)
+    posicion_x = round((60-len(cadena))/2)
+    Vent.addstr(0,posicion_x,cadena)
+
+#Muestra Pantalla
+Pintado_Menu(window)
+while opcion==0:
+    #obtenemos posicion del menu
+    opcion= window.getch()
+    if (opcion == 49):
+        #opcion 1
+        opcion=0
+    elif (opcion==52):
+        #opcion 4 Salir
+        opcion = 100
+    else:
+        opcion=0
+
+
+curses.endwin() #Cierra ventanas
---
+++ [EDD]P2_201701133/venv/Scripts/pip3-script.py
@@ -0,0 +1,12 @@
+#!"C:\Users\Aragon Perez\Documents\Estructura de Datos\EDD_2S2019_P2_201701133\[EDD]P2_201701133\venv\Scripts\python.exe"
+# EASY-INSTALL-ENTRY-SCRIPT: 'pip==19.0.3','console_scripts','pip3'
+__requires__ = 'pip==19.0.3'
+import re
+import sys
+from pkg_resources import load_entry_point
+
+if __name__ == '__main__':
+    sys.argv[0] = re.sub(r'(-script\.pyw?|\.exe)?$', '', sys.argv[0])
+    sys.exit(
+        load_entry_point('pip==19.0.3', 'console_scripts', 'pip3')()
+    )
